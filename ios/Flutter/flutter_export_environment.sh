052752020c400909af8cd7d279406e19988849ec
Sajal <sajaldharm78@gmail.com>
2024-02-03 10:54:05 +0530

    Add 15601012022's new feature

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\meena\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\__Sakshi\Flutter_Course\mychat_app"
+export "FLUTTER_ROOT=C:\Users\salal\Downloads\flutter_windows_3.16.7-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\_flutter\Mobile-development"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
